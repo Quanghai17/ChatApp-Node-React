7987a815396357a70188d2ba49e7a3bf9d08ac85
vanhuy174 <vanhuy174@gmail.com>
2024-06-19 17:17:16 +0700

    19/5

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,30 @@
 import { Outlet } from "react-router-dom"
-
+import axios from 'axios'
+import { useEffect } from "react"
 
 const Home = () => {
+    const fetchUserDetails = async () => {
+        try {
+            const URL = `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/api/user-details`
+
+            const response = await axios({
+                url: URL,
+                withCredentials: true,
+            })
+
+            console.log("User details: ", response)
+        } catch (error) {
+            console.log("error", error)
+        }
+    }
+
+    useEffect(() => {
+        fetchUserDetails()
+    }, [])
+
     return (
         <div>Home
-           
+
             <section>
                 <Outlet />
             </section>
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { IoClose } from "react-icons/io5";
 import uploadFile from '../helpers/uploadFile';
@@ -13,152 +13,153 @@ const RegisterPage = () => {
     profile_pic: "",
   });
 
-  const [uploadPhoto , setUploadPhoto] = useState("")
+  const [uploadPhoto, setUploadPhoto] = useState("")
+  const navigate = useNavigate()
 
-  const handleOnChange = (e)=>{
-    const { name, value} = e.target
+  const handleOnChange = (e) => {
+    const { name, value } = e.target
 
-    setData((preve)=>{
-      return{
-          ...preve,
-          [name] : value
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value
       }
     })
   }
 
-  const handleUploadPhoto = async(e)=>{
+  const handleUploadPhoto = async (e) => {
     const file = e.target.files[0]
 
     const uploadPhoto = await uploadFile(file)
     setUploadPhoto(file)
 
-    setData((preve)=>{
-      return{
+    setData((preve) => {
+      return {
         ...preve,
-        profile_pic : uploadPhoto?.url
+        profile_pic: uploadPhoto?.url
       }
     })
   }
 
-  const handleClearUploadPhoto = (e)=>{
+  const handleClearUploadPhoto = (e) => {
     e.preventDefault()
     e.stopPropagation()
     setUploadPhoto("null")
   }
 
-  const handleSubmit = async (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault()
     e.stopPropagation()
 
     const URL = `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/api/register`
-    console.log(URL)
+
     try {
-      const response = await axios.post(URL,data)
+      const response = await axios.post(URL, data)
       toast.success(response.data.message)
 
-      if(response.data.success){
-          setData({
-            name : "",
-            email : "",
-            password : "",
-            profile_pic : ""
-          })
+      if (response.data.success) {
+        setData({
+          name: "",
+          email: "",
+          password: "",
+          profile_pic: ""
+        })
 
-          // navigate('/email')
+        navigate('/email')
 
       }
-  } catch (error) {
-    console.log(error)
+    } catch (error) {
+      console.log(error)
       toast.error(error?.response?.data?.message)
-  }
-  console.log('data',data)
+    }
+  
   }
 
   return (
     <div className='mt-5'>
-        <div className='bg-white w-full max-w-md  rounded overflow-hidden p-4 mx-auto'>
-          <h3>Welcome to Chat app!</h3>
+      <div className='bg-white w-full max-w-md  rounded overflow-hidden p-4 mx-auto'>
+        <h3>Welcome to Chat app!</h3>
 
-          <form className='grid gap-4 mt-5' onSubmit={handleSubmit}>
-              <div className='flex flex-col gap-1'>
-                <label htmlFor='name'>Name :</label>
-                <input
-                  type='text'
-                  id='name'
-                  name='name'
-                  placeholder='enter your name' 
-                  className='bg-slate-100 px-2 py-2 focus:outline-primary rounded-lg'
-                  value={data.name}
-                  onChange={handleOnChange}
-                  required
-                />
+        <form className='grid gap-4 mt-5' onSubmit={handleSubmit}>
+          <div className='flex flex-col gap-1'>
+            <label htmlFor='name'>Name :</label>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              placeholder='enter your name'
+              className='bg-slate-100 px-2 py-2 focus:outline-primary rounded-lg'
+              value={data.name}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <label htmlFor='email'>Email :</label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              placeholder='enter your email'
+              className='bg-slate-100 px-2 py-2 focus:outline-primary rounded-lg'
+              value={data.email}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <label htmlFor='password'>Password :</label>
+            <input
+              type='password'
+              id='password'
+              name='password'
+              placeholder='enter your password'
+              className='bg-slate-100 px-2 py-2 focus:outline-primary rounded-lg'
+              value={data.password}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <label htmlFor='profile_pic'>Photo :
+
+              <div className='h-14 bg-slate-200 flex justify-center items-center border rounded hover:border-primary cursor-pointer rounded-lg'>
+                <p className='text-sm max-w-[300px] text-ellipsis line-clamp-1'>
+                  {
+                    uploadPhoto?.name ? uploadPhoto?.name : "Upload profile photo"
+                  }
+                </p>
+                {
+                  uploadPhoto?.name && (
+                    <button className='text-lg ml-2 hover:text-red-600' onClick={handleClearUploadPhoto}>
+                      <IoClose />
+                    </button>
+                  )
+                }
               </div>
 
-              <div className='flex flex-col gap-1'>
-                <label htmlFor='email'>Email :</label>
-                <input
-                  type='email'
-                  id='email'
-                  name='email'
-                  placeholder='enter your email' 
-                  className='bg-slate-100 px-2 py-2 focus:outline-primary rounded-lg'
-                  value={data.email}
-                  onChange={handleOnChange}
-                  required
-                />
-              </div>
+            </label>
 
-              <div className='flex flex-col gap-1'>
-                <label htmlFor='password'>Password :</label>
-                <input
-                  type='password'
-                  id='password'
-                  name='password'
-                  placeholder='enter your password' 
-                  className='bg-slate-100 px-2 py-2 focus:outline-primary rounded-lg'
-                  value={data.password}
-                  onChange={handleOnChange}
-                  required
-                />
-              </div>
-
-              <div className='flex flex-col gap-1'>
-                <label htmlFor='profile_pic'>Photo :
-
-                  <div className='h-14 bg-slate-200 flex justify-center items-center border rounded hover:border-primary cursor-pointer rounded-lg'>
-                  <p className='text-sm max-w-[300px] text-ellipsis line-clamp-1'>
-                        {
-                          uploadPhoto?.name ? uploadPhoto?.name : "Upload profile photo"
-                        }
-                      </p>
-                      {
-                        uploadPhoto?.name && (
-                          <button className='text-lg ml-2 hover:text-red-600' onClick={handleClearUploadPhoto}>
-                            <IoClose/>
-                          </button>
-                        )
-                      }
-                  </div>
-                
-                </label>
-                
-                <input
-                  type='file'
-                  id='profile_pic'
-                  name='profile_pic'
-                  className='bg-slate-100 px-2 py-1 focus:outline-primary hidden'
-                  onChange={handleUploadPhoto}
-                />
-              </div>
-              <button
-              type='submit'
-               className='bg-primary text-lg  px-4 py-1 hover:bg-secondary rounded mt-2 font-bold text-white leading-relaxed tracking-wide rounded-lg' 
-              >
-                Register
-              </button>
-          </form>
-          <p className='my-3 text-center'>Already have account ? <Link to={"/email"} className='hover:text-primary font-semibold'>Login</Link></p>
-        </div>
+            <input
+              type='file'
+              id='profile_pic'
+              name='profile_pic'
+              className='bg-slate-100 px-2 py-1 focus:outline-primary hidden'
+              onChange={handleUploadPhoto}
+            />
+          </div>
+          <button
+            type='submit'
+            className='bg-primary text-lg  px-4 py-1 hover:bg-secondary rounded mt-2 font-bold text-white leading-relaxed tracking-wide rounded-lg'
+          >
+            Register
+          </button>
+        </form>
+        <p className='my-3 text-center'>Already have account ? <Link to={"/email"} className='hover:text-primary font-semibold'>Login</Link></p>
+      </div>
     </div>
   )
 }
